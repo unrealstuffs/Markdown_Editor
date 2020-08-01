12a3006cc4de714214be90b78180646fe06c989c
unrealstuffs <slozhna0@gmail.com>
2020-08-01 22:25:12 +0500

    Updates

---
+++ precache-manifest.8c1a268563d40841dc5defa12a0c31fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d6559a1d2fa75b531ebdbaa0fbf9da",
+    "url": "/Markdown_Editor/index.html"
+  },
+  {
+    "revision": "37d3c1cc7b81383fc6c1",
+    "url": "/Markdown_Editor/static/css/main.1c1b6713.chunk.css"
+  },
+  {
+    "revision": "0405e1840fc782ad456f",
+    "url": "/Markdown_Editor/static/js/2.a5baa5b7.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/Markdown_Editor/static/js/2.a5baa5b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d3c1cc7b81383fc6c1",
+    "url": "/Markdown_Editor/static/js/main.2ee7680d.chunk.js"
+  },
+  {
+    "revision": "8b878027f0ed39daeeb5",
+    "url": "/Markdown_Editor/static/js/runtime-main.0276cc17.js"
+  }
+]);
